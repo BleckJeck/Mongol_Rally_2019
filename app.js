e41cfb16641e747af26c1807e0fd2b9d2a5713b1
Giacomo Carnovalini <bleckjeck90@gmail.com>
2019-02-09 19:44:17 +0000

    updated fAQ layout

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const app = new Vue({
   el: '#app',
   data: {
-    test: 'experiment'
+    
   },
   methods: {
-
-  },
-  computed: {
-  //   filteredList() {
-  //     return this.recipesList.filter((recipe) => {
-  //       return recipe.name.includes(this.searchInput);
-  //     });
-  //   }
+    
   },
   mounted() {
-    // this.fetchRecipes()
- }
+    
+  }
 })
